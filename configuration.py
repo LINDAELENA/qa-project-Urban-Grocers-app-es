07e0dac4739044a11f931148847c5c721a03fa3d
LINDAELENA <lc-herrera@outlook.com>
2024-06-02 23:58:41 -0600

    Se almacena ruta para crear un nuevo usuario

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
+URL_SERVICE = "https://cnt-f1f5cce0-3b9a-4433-b034-ecfdfd89270b.containerhub.tripleten-services.com"# Inserta tu dirección de URL sin la barra diagonal al final
+CREATE_USER_PATH = "/api/v1/users/"# Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = # Almacena la ruta para crear un kit en esta variable
