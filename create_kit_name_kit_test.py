3a112e0bee9cdee57e766fb2e0a118cbe4ff2710
LINDAELENA <lc-herrera@outlook.com>
2024-06-03 20:17:06 -0600

    Se define función para cambiar contenido del cuerpo de solicitud

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -1,3 +1,8 @@
 import sender_stand_request
 import data
 
+def get_kit_body (kit_name):  #Función para cambiar el cuerpo del contenido de la solicitud
+    headers_copy = data.headers.copy()  #Crea copia de los headers para no modificar original
+    current_kit_body = data.kit_name.copy()  #Copia el diccionario con el cuerpo de la solicitud desde data
+    current_kit_body["name"] = kit_name  #Cambia el valor del parámetro name
+    return current_kit_body, headers_copy  #Devuelve un nuevo diccionario con el nombre
